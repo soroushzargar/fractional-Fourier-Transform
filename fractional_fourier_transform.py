e2c4e65468108e2e5abf0dd32837fcfe8b5d0fb1
soroushzargar <soroushzargar@gmail.com>
2020-01-30 16:11:47 +0330

    Four primary functions are implemented, works with librosa

--- fractional_fourier_transform.py
+++ fractional_fourier_transform.py
@@ -3,15 +3,24 @@ import scipy.linalg as LinAlg
 
 
 class FractionalFourierTransform:
-    fourierMatrix = None
+    precalculated_fourierMatrix = None
+    precalculated_fractionalTransform = None
+    precalculated_inverseTransform = None
     signalLen = None
+    alpha = None
 
-    def __init__(self):
+    def __init__(self, alpha=1, signalLen=None):
         super().__init__()
+
+        if signalLen is not None:
+            self.signalLen = signalLen
+
+        if alpha is not None:
+            self.alpha = alpha
 
     def makeFourierMatrix(self):
         N = self.signalLen
-        self.fourierMatrix = N * np.array([[
+        self.precalculated_fourierMatrix = N * np.array([[
             np.exp(
                 1j * ((-2.0 * np.pi) * (ii) * (jj)) / np.fix(N)
                 ) / np.fix(N)
@@ -21,9 +30,33 @@ class FractionalFourierTransform:
         )
         return
 
-    def frft(self, y, alpha=1, n=None, axis=-1, norm=None):
-        # # Since the transformation is Periodic with p=4
-        # alpha = np.remainder(alpha, 4.0)
+    def fft(self, y, n=None, axis=-1, norm=None):
+        # This line ensures that the input is a numpy array
+        y = np.array(y)
+
+        if n is None:
+            n = y.shape[axis]
+
+        y = y[:n]
+
+        if self.signalLen is None:
+            self.signalLen = n
+
+        # Class Initial Checks
+        if self.signalLen != n:
+            self.signalLen = n
+            self.precalculated_fourierMatrix = None
+            self.precalculated_fractionalTransform = None
+
+        if self.precalculated_fourierMatrix is None:
+            self.makeFourierMatrix()
+        if self.precalculated_fractionalTransform is None:
+            self.precalculated_fractionalTransform = \
+                LinAlg.fractional_matrix_power(
+                    self.precalculated_fourierMatrix, self.alpha
+                )
+
+        # On this stage, the precalculated matrices are ready
 
         # To perform complex arithmetic, y should be complex
         complexInput = y.copy().astype(np.complex128)
@@ -31,50 +64,61 @@ class FractionalFourierTransform:
         # Creating output container
         output = np.zeros_like(complexInput)
 
-        # Getting Length of the Signal
-        self.signalLen = complexInput.shape[0]
-        # Checking the Fourier Transform Matrix
-        if self.fourierMatrix is None:
-            self.makeFourierMatrix()
-        elif self.fourierMatrix.shape[0] != self.signalLen:
-            self.makeFourierMatrix()
+        print(self.precalculated_fractionalTransform.shape)
+        print(np.transpose(complexInput).shape)
 
-        # Taking the fourier matrix to fractional power
-        fractionalFTMatrix = LinAlg.fractional_matrix_power(
-            self.fourierMatrix, alpha
-        )
         output = np.matmul(
-            fractionalFTMatrix,
+            self.precalculated_fractionalTransform,
+            complexInput
+        )
+        return output
+
+    def rfft(self, y, n=None, axis=-1, norm=None):
+        result = self.fft(y, n, axis, norm)
+        N = result.shape[axis]
+        return result[:(N//2 + 1)]
+
+    def ifft(self, y, n=None, axis=-1, norm=None):
+        # This line ensures that the input is a numpy array
+        y = np.array(y)
+
+        if n is None:
+            n = y.shape[axis]
+
+        y = y[:n]
+
+        if self.signalLen is None:
+            self.signalLen = n
+
+        # Class Initial Checks
+        if self.signalLen != n:
+            self.signalLen = n
+            self.precalculated_fourierMatrix = None
+            self.precalculated_inverseTransform = None
+
+        if self.precalculated_fourierMatrix is None:
+            self.makeFourierMatrix()
+        if self.precalculated_inverseTransform is None:
+            self.precalculated_inverseTransform = \
+                LinAlg.fractional_matrix_power(
+                    self.precalculated_fourierMatrix, -1 * self.alpha
+                )
+
+        # On this stage, the precalculated matrices are ready
+
+        # To perform complex arithmetic, y should be complex
+        complexInput = y.copy().astype(np.complex128)
+
+        # Creating output container
+        output = np.zeros_like(complexInput)
+
+        output = np.matmul(
+            self.precalculated_inverseTransform,
             np.transpose(complexInput)
         )
         return output
-    
-    
-    def rfrft(self, y, alpha=1, n=None, axis=-1, norm=None):
-        # # Since the transformation is Periodic with p=4
-        # alpha = np.remainder(alpha, 4.0)
 
-        # To perform complex arithmetic, y should be complex
-        complexInput = y.copy().astype(np.complex128)
-
-        # Creating output container
-        output = np.zeros_like(complexInput)
-
-        # Getting Length of the Signal
-        self.signalLen = complexInput.shape[0]
-        # Checking the Fourier Transform Matrix
-        if self.fourierMatrix is None:
-            self.makeFourierMatrix()
-        elif self.fourierMatrix.shape[0] != self.signalLen:
-            self.makeFourierMatrix()
-
-        # Taking the fourier matrix to fractional power
-        fractionalFTMatrix = LinAlg.fractional_matrix_power(
-            self.fourierMatrix, alpha
-        )
-        output = np.matmul(
-            fractionalFTMatrix,
-            np.transpose(complexInput)
-        )
-        SymmetricOutput = output[:self.signalLen//2 + 1]
-        return SymmetricOutput
+    def irfft(self, y, n=None, axis=-1, norm=None):
+        y_rev = np.flip(y[1:], axis)
+        yConventional = np.concatenate((y, y_rev), axis=axis)
+        return self.ifft(yConventional, n, axis, norm)
